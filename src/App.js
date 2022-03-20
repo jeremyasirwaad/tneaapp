ec2709c797fbcf950ac4f6f0d0521cb7c139e210
Jeremy <asirwaadj@gmail.com>
2022-03-21 01:51:49 +0530

    added text files

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import data from "./data.xml";
+import sha256 from "crypto-js/sha256";
+// import cryptoRandomString from 'crypto-random-string';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+
+
+	useEffect(() => {
+		// const getdata = async () => {
+		// 	const data = await fetch(
+		// 		"auth.uidai.gov.in/2.0/public/9/9/MMxNu7a6589B5x5RahDW-zNP7rhGbZb5HsTRwbi-VVNxkoFmkHGmYKM",{
+		//       headers: {
+		//         'Content-Type': 'application/xml'
+		//       },
+		//       body : {
+
+		//       }
+		//     }
+		// 	)
+		// 		.then((res) => console.log(res))
+		// 		.then((asd) => console.log(data));
+		// };
+
+		// getdata();
+		// console.log(cryptoRandomString({length: 10, type: 'base64'}));
+		console.log(sha256("Message"));
+		var xhr = new XMLHttpRequest();
+		xhr.open(
+			"POST",
+			"https://thingproxy.freeboard.io/fetch/http://auth.uidai.gov.in/2.0/public/9/9/MMxNu7a6589B5x5RahDW-zNP7rhGbZb5HsTRwbi-VVNxkoFmkHGmYKM",
+			true
+		);
+		xhr.setRequestHeader("Content-Type", "text/xml");
+		xhr.send(data);
+	}, []);
+
+	return (
+		<div className="App">
+			<h1>Hello</h1>
+		</div>
+	);
 }
 
 export default App;
